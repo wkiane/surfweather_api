ab50920d560207f0b1591a87805132d01f714cfd
willa <willa_abreu@outlook.com>
2020-10-08 23:27:31 -0300

    setup prettier

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,33 @@
-import './util/module-alias'
+import './util/module-alias';
 
-import {Application} from 'express'
+import { Application } from 'express';
 
-import {Server} from '@overnightjs/core';
+import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 
-import {ForecastController} from './app/controllers/forecast'
+import { ForecastController } from './app/controllers/forecast';
 
 export class SetupServer extends Server {
-    constructor(private port = 3333) {
-        super();
-    }
+  constructor(private port = 3333) {
+    super();
+  }
 
-    public int(): void {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  public int(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    private setupControllers(): void {
-        const forecastController = new ForecastController();
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
 
-        this.addControllers([forecastController])
-    }
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,9 +1,9 @@
-import { SetupServer } from '@src/server'
+import { SetupServer } from '@src/server';
 
 import supertest from 'supertest';
 
 beforeAll(() => {
-    const server = new SetupServer();
-    server.int();
-    global.testRequest = supertest(server.getApp());
+  const server = new SetupServer();
+  server.int();
+  global.testRequest = supertest(server.getApp());
 });
