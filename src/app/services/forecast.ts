c0c6004b0cda515e56bb1567f6bc745efa490767
willa <willa_abreu@outlook.com>
2020-10-14 22:39:59 -0300

    create forecast service

---
+++ src/app/services/forecast.ts
@@ -0,0 +1,42 @@
+import { StormGlass, ForecastPoint } from '@src/app/clients/stormGlass';
+
+export enum BeachPosition {
+    S = 'S',
+    E = 'E',
+    W = 'W',
+    N = 'N',
+}
+
+export interface Beach {
+    lat: number;
+    lng: number;
+    name: string;
+    position: BeachPosition;
+    user: string;
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class Forecast {
+  constructor (protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches (beaches: Beach[]): Promise<BeachForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = [];
+
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map(e => ({
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1
+        },
+        ...e
+      }));
+      pointsWithCorrectSources.push(...enrichedBeachData);
+    }
+    return pointsWithCorrectSources;
+  }
+}
