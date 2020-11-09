4315d3d3a76cd2f04f02347a73a545905b68584d
willa <willa_abreu@outlook.com>
2020-11-08 22:58:50 -0300

    create me endpoint with user information

--- src/app/controllers/users.ts
+++ src/app/controllers/users.ts
@@ -1,8 +1,15 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Post, Get, Middleware } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/app/models/user';
 import { BaseController } from '.';
 import AuthService from '../services/auth';
+import { authMiddleware } from '@src/app/middlewares/auth';
+
+export interface UserResponse {
+  name: string;
+  email: string;
+  password?: string;
+}
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -39,5 +46,23 @@ export class UsersController extends BaseController {
 
     const token = AuthService.generateToken(user.toJSON());
     return res.status(200).send({ token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me (req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email }) as UserResponse;
+
+    user && delete user.password;
+
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!'
+      });
+    }
+
+    return res.send({ user });
   }
 }
